2f1ef74a7989c1bbc2654eb96f2a8dbf1b92d4ce
Lucas Combin <lucascombin@gmail.com>
2023-11-01 16:39:33 -0300

    Correcion en los vhd del bloque TOP y su tb

--- src/MatrixModulator_top.vhd
+++ src/MatrixModulator_top.vhd
@@ -5,9 +5,7 @@ use IEEE.STD_LOGIC_UNSIGNED.ALL;
 entity MatrixModulator_top is
     port (
         clk   : in std_logic;
-        reset : in std_logic;
-        
-    );
+        reset : in std_logic );
 end entity;
 
 architecture Behavioral of MatrixModulator_top is
--- tb/MatrixModulator_tb.vhd
+++ tb/MatrixModulator_tb.vhd
@@ -10,6 +10,22 @@ ENTITY MatrixModulator_tb IS
 END MatrixModulator_tb;
 
 architecture Behavioral of MatrixModulator_tb is
-    begin
+
+    component MatrixModulator_top
+        port(
+        clk   : in std_logic;
+        reset : in std_logic );
+    end component;
+    
+    signal CLK : std_logic;
+    signal RESET : std_logic;
+
+
+begin
+    
+    uut: MatrixModulator_top PORT MAP (
+        clk => CLK,
+        reset => RESET        
+    );
     
 end;
